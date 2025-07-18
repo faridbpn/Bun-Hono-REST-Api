f34e4048373a10e451ded7a617f8eafb9d6ebd6d
faridbpn <farid.idnsolo@gmail.com>
2025-07-18 15:27:26 +0800

    define user type for get user api function

--- src/controller/user-controller.ts
+++ src/controller/user-controller.ts
@@ -1,8 +1,9 @@
 import { Hono } from "hono";
 import { RegisterUserRequest, LoginUserRequest } from "../model/user-model";
 import { UserService } from "../service/user-service";
+import { ApplicationVariables } from "../model/app-model";
 
-export const userController = new Hono();
+export const userController = new Hono<{Variables: ApplicationVariables}>();
 
 userController.post('/api/users', async (c) => {
     const request = await c.req.json() as RegisterUserRequest;
@@ -26,4 +27,19 @@ userController.post('/api/users/login', async (c) => {
     return c.json({
         data: response
     })
+})
+
+// kita mau pasang middleware bang
+userController.use(async (c, next) => {
+    const token = c.req.header('Authorization')
+    const user  = await UserService.get(token)
+
+    c.set('user', user)
+
+    await next()
+}) 
+
+userController.get('/api/users/current', async (c) => {
+    //ambil user
+
 })
